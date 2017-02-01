978d4ffeee73089902f9684b55a9ba50bf52f110
Sasha Y <ssyamada@hawaii.edu>
2017-02-01 13:15:21 -1000

    Add files via upload
    almost done - need to implement a flag to end the program

--- life.cpp
+++ life.cpp
@@ -4,30 +4,38 @@
 
 #define ORG 1
 #define DEAD 0
+#define ISOLATION 2
+#define JUST_RIGHT 3
+#define DONE 5
+
 #include <iostream>
 
 using namespace std;
 
 void printgrid(int grid[10][10]);
-void checkneighbors(int (*grid)[10][10]);
+int checkneighbors(int (&grid)[10][10]);
 
 int main(){
     int life_grid[10][10];
-    int (*p_grid)[10][10] = &life_grid;
+    int (&p_grid)[10][10] = life_grid;
     int x, y;
-    int action;
+    int i_org = 0;
+    int status = 0;
 
+    //ask for number of starting organisms
+    cout << "How many initial organisms?" << endl;
+    cin >> i_org;
 
     //initialize game of life matrix
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
-            life_grid[i][j] = 0;
-            life_grid[i][j] = 0;
+            life_grid[i][j] = DEAD;
+            life_grid[i][j] = DEAD;
         }
     }
     
-    //spawn first org in random location
-    for(int i = 0; i < 8; i++){
+    //spawn first orgs in random locations
+    for(int i = 0; i < i_org; i++){
         x = rand() % 10;
         y = rand() % 10;
 
@@ -39,15 +47,15 @@ int main(){
     cout << endl;
 
     //an org is born in empty cells with exactly 3 neighbors
-    //check for neighbors
-    
-    checkneighbors(p_grid); 
-
     //an org dies if it has less than 2 neighbors
-    //check for neighbors
-    
     //an org dies if it has more than 3 neighbors
     //check for neighbors
+    
+    status = checkneighbors(p_grid); 
+
+    //print out new grid
+    printgrid(life_grid);
+    cout << endl;
 
 }
 
@@ -62,30 +70,38 @@ void printgrid(int grid[10][10]){
     }
 }
 
-void checkneighbors(int (*grid)[10][10]){
+int checkneighbors(int (&grid)[10][10]){
     int neighbors = 0;
+    int flag = 0;
 
     for (int i = 0; i < 10; ++i){
         for (int j = 0; j < 10; ++j){
             //find neighbors
-            if(*grid[i][j] == ORG){
-                if(*grid[i-1][j-1] == ORG) neighbors++;            
-                if(*grid[i-1][j] == ORG) neighbors++;            
-                if(*grid[i][j-1] == ORG) neighbors++;            
-                if(*grid[i-1][j+1] == ORG) neighbors++;            
-                if(*grid[i+1][j-1] == ORG) neighbors++;            
-                if(*grid[i+1][j] == ORG) neighbors++;            
-                if(*grid[i][j+1] == ORG) neighbors++;            
-                if(*grid[i+1][j+1] == ORG) neighbors++;            
-            }
-            //implement rules of life
-            if(neighbors == 3) *grid[i][j] = ORG;
-            else if(neighbors < 2) *grid[i][j] = DEAD;
-            else if(neighbors > 3) *grid[i][j] = DEAD;
+                if(grid[i-1][j-1] == ORG) neighbors++;            
+                if(grid[i-1][j] == ORG) neighbors++;            
+                if(grid[i][j-1] == ORG) neighbors++;            
+                if(grid[i-1][j+1] == ORG) neighbors++;            
+                if(grid[i+1][j-1] == ORG) neighbors++;            
+                if(grid[i+1][j] == ORG) neighbors++;            
+                if(grid[i][j+1] == ORG) neighbors++;            
+                if(grid[i+1][j+1] == ORG) neighbors++;            
             
+            //implement rules of life
+            //new org spawned
+            if(neighbors == JUST_RIGHT) grid[i][j] = ORG;
+            //org dies
+            else if(neighbors < ISOLATION) grid[i][j] = DEAD;
+            else if(neighbors > JUST_RIGHT) grid[i][j] = DEAD;
+            else if(neighbors == 2){
+            //do something about flagging
+            };
+
             //reset neighbors
             neighbors = 0;
         }
     }
+    if(flag == 0) return DONE;
+    else return 0;
 }
+
 
