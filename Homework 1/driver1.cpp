793407a3d17f3e073684adfb420f67970b51b9ab
Sean Teramae <seteramae@gmail.com>
2017-01-31 20:26:22 -1000

    Got array working (hopefully)

--- Homework 1/driver1.cpp
+++ Homework 1/driver1.cpp
@@ -37,7 +37,7 @@ int main(){
   // Assign Temperatures
   for(int i = 0; i < n; ++i){
     for(int j = 0; j < n; ++j){
-      if(i*n+j == 1*n+1) grid[i*n+j] = tin;     // First Inner
+      if(i*n+j == 1*n+1) grid[i*n+j] = tin;       // First Inner
       if(i == 0) grid[i*n+j] = t1;                // Top side
       else if(j == n-1) grid[i*n+j] = t2;         // Right side
       else if(i == n-1) grid[i*n+j] = t3;         // Bottom side
@@ -46,11 +46,10 @@ int main(){
   }
 
   // Calculate
-  // it = avgGrid(grid, n);
+  it = avgGrid(grid, n);
 
   cout << it << " iterations" << endl;
   for(int i = 0; i < n*n; ++i){
-
     if(i%n == 0 && i != 0) cout << endl;
     cout << grid[i] << " ";
   }
--- Homework 1/stats.cpp
+++ Homework 1/stats.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int avgGrid(int array[], int size){
   int iterator = 0, change = 2, old;
-  while(change > 1){
-    for(int i = 1; i < size; ++i){
+  while(change > 1 || change < -1){
+    old = array[1*size+1];
+    for(int i = 1; i < size-1; ++i){
       for(int j = 1; j < size; ++ j){
-        old = array[i*size+j];
-        array[i*size+j] = (array[i*size+j-1]+array[i*size+j+1]+array[i*(size+1)+j]+array[i*(size-1)+j])/4;
-        change = array[i*size+j] - old;
+        //old = array[i*size+j];
+        array[i*size+j] = (array[i*size+j-1]+array[i*size+j+1]+array[(i+1)*size+j]+array[(i-1)*size+j])/4;
+        //change = array[i*size+j] - old;
       }
     }
     iterator += 1;
+    change = array[1*size+1] - old;
+    cout << change << endl;
   }
   return iterator;
 }
