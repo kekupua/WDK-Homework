f4d04f159e54108e027f0fb4e86494a54cb8e872
Riley Cammack <rcammack@hawaii.edu>
2017-04-12 11:58:11 -1000

    tamagotchi.h

---
+++ Final Project/tamagotchi.h
@@ -0,0 +1,16 @@
+
+//tamagotchi.h
+
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+//tamagotchi class
+class tama {
+string name;
+int age;
+int hunger;
+int health;
+int mood;
+tama() {name = ""; age = 0; hunger = 0; health = 0; mood = 0;};
+};
