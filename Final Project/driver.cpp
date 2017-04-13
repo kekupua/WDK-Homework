30912ae5ee6c63c67cbf942f898d4b26a47d9b16
Riley Cammack <rcammack@hawaii.edu>
2017-04-12 15:25:35 -1000

    Create driver.cpp

---
+++ Final Project/driver.cpp
@@ -0,0 +1,10 @@
+#include "display.cpp"
+#include <string.h>
+using namespace std;
+
+int main() {
+display("egg.txt", "egg2.txt");
+display("rabbit.txt", "rabbit2.txt");
+display("potato.txt", "potato2.txt");
+}
+~       
