922a1bfa7b666f47970f8da91400b2f4a2ea31b7
Riley Cammack <rcammack@hawaii.edu>
2017-04-12 15:44:49 -1000

    Update display.cpp

--- Final Project/display.cpp
+++ Final Project/display.cpp
@@ -19,22 +19,33 @@ int display(string x, string y){
     clrscr(); //clear screen
     
     ifstream in5(x);
+    ifstream in7("status.txt");
     char s[255];
 
     while(in5){
       in5.getline(s, 255);
       if(in5) cout << s << endl;
     }
-    
+
+    while(in7){
+      in7.getline(s, 255);
+      if(in7) cout << s << endl;
+    }
+
     usleep(400000);
     clrscr(); //clear screen
-    
+
     ifstream in6(y);
+    ifstream in8("status.txt");
     while(in6){
       in6.getline(s, 255);
       if(in6) cout << s << endl;
     }
 
+    while(in8){
+      in8.getline(s, 255);
+      if(in8) cout << s << endl;
+    }
   }
 
 return 0;
