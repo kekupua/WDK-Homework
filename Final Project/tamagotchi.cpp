3b367a936e4ee9096dc30c34ae4d4304378ee2b9
Sean Teramae <seteramae@gmail.com>
2017-04-19 18:24:24 -1000

    Basic Shop Function

--- Final Project/tamagotchi.cpp
+++ Final Project/tamagotchi.cpp
@@ -3,6 +3,37 @@ bool tama::playGame(){
 
 }
 
+void tama::shop(){
+  int input;
+  cout << "What do you want to buy?" << endl;
+  cout << "(1) Medicine [+1 health]\n(2) Bread[+1 health]\n(3) Candy[+1 mood, -1 health]\n";
+  cin >> input;
+  if(getMoney() == 0){
+    cout << "You have no Money!" << endl;
+  }
+  else{
+    if(input == 1){ // Medicine
+      setMedC(getMedC()+1);
+      setMoney(getMoney()-1);
+      cout << "Bought Medicine!\n";
+    }
+    else if(input == 2){ // Bread
+      setBreadC(getBreadC()+1);
+      setMoney(getMoney()-1);
+      cout << "Bought Bread!\n";
+    }
+    else if(input == 3){ // Candy
+      setCandyC(getCandyC()+1);
+      setMoney(getMoney()-1);
+      cout << "Bought Candy!\n";
+    }
+    else{
+      cout << "Invalid input!" << endl;
+    }
+  }
+
+}
+
 void tama::feed(){
   int input;
   cout << "What do you want to feed?" << endl;
